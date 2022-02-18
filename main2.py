d3fd0656b021ccb8a68546d00652efec57076810
Amaljith <amaljith64@gmail.com>
2022-02-18 18:32:06 +0530

    method BLUR for image

--- main2.py
+++ main2.py
@@ -9,4 +9,9 @@ import numpy as np
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-
+image=cv2.imread("bed.jpg",1)
+blur=cv2.blur(image,(5,5))
+cv2.imshow('image',image)
+cv2.imshow('blur',blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
